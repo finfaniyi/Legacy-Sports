1fe3bb5670fc6772ef43eb82dca87ef6b9fa1c18
Fin Faniyi <ffaniyi@uwo.ca>
2026-02-16 18:21:43 -0500

    Update static and template files

--- mysite/settings.py
+++ mysite/settings.py
@@ -63,28 +63,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # DATABASE
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
-
-if os.environ.get("RENDER"):
-    # Production database (still SQLite for you)
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    # Local development
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
 
 # PASSWORD VALIDATION
 AUTH_PASSWORD_VALIDATORS = [
