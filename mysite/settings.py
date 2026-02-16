2552aafb3073386250f45c16c0212ef1856023b9
Fin Faniyi <ffaniyi@uwo.ca>
2026-02-16 18:57:06 -0500

    Update static and template files

--- mysite/settings.py
+++ mysite/settings.py
@@ -62,13 +62,21 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # DATABASE
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
+if os.environ.get("DATABASE_URL"):
+    DATABASES = {
+        'default': dj_database_url.config(
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 # PASSWORD VALIDATION
 AUTH_PASSWORD_VALIDATORS = [
