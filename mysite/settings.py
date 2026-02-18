481a99d286e2d023aa5eecf21b17272b85444335
Fin Faniyi <ffaniyi@uwo.ca>
2026-02-18 15:27:41 -0500

    Update static and template files

--- mysite/settings.py
+++ mysite/settings.py
@@ -115,14 +115,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #Emails
 import os
+EMAIL_BACKEND = "sendgrid_backend.SendgridBackend"
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-
-EMAIL_HOST = "smtp.sendgrid.net"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-
-EMAIL_HOST_USER = "apikey"
-EMAIL_HOST_PASSWORD = os.environ.get("SENDGRID_API_KEY")
+SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY")
 
 DEFAULT_FROM_EMAIL = "noreply@legacysportscanada.ca"
+
