f723d894df8e2173d184dedfc62e97e2515d4128
Fin Faniyi <ffaniyi@uwo.ca>
2026-02-18 14:12:04 -0500

    Update static and template files

--- mysite/settings.py
+++ mysite/settings.py
@@ -114,10 +114,13 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #Emails
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_HOST = "smtp.sendgrid.net"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'legacysportscanada@gmail.com'
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = 'Legacy Sports <info@legacysportscanada.ca>'
+
+EMAIL_HOST_USER = "apikey"  # literally this word
+EMAIL_HOST_PASSWORD = os.environ.get("SENDGRID_API_KEY")
+
+DEFAULT_FROM_EMAIL = "legacysportscanada@gmail.com"
